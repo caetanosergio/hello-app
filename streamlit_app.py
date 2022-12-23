383683ab815a4ee778c30b593735def46c41d23b
caetanosergio <89207444+caetanosergio@users.noreply.github.com>
2022-12-23 17:59:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,3 @@ import streamlit as st
 st.write("Hello World")
 
 st.title('Handong Bible App')
-
-st.header("This is the Heading")
-
-st.subheading("This is the subheading")
-
-st.text("Hello this is Ale's Bible")
-

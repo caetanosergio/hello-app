16ddc42ebbc0c499ca49df30bb06852ca667955b
caetanosergio <89207444+caetanosergio@users.noreply.github.com>
2022-12-23 17:56:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,13 +15,3 @@ st.subheading("This is the subheading")
 #text
 st.text("Hello this is Ale's Bible")
 
-# success, info
-st.sucess("Executed successfully")
-st.info("This is an information")
-st.warning("This is a warning")
-st.error("An error occured")
-channel = "iakdskjfdkj"
-st.write("Subscribe to my channel", channel)
-
-#Text input box
-name = st.text_input("Enter the Bible name", "Bible Name")

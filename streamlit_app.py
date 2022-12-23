97819e7aea74912497339b02dd2bf89053e3d68d
caetanosergio <89207444+caetanosergio@users.noreply.github.com>
2022-12-23 17:55:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,27 @@
 import streamlit as st
 
 st.write("Hello World")
+
+
+# Title
+st.title('Handong Bible App')
+
+# Header
+st.header("This is the Heading")
+
+# Subh
+st.subheading("This is the subheading")
+
+#text
+st.text("Hello this is Ale's Bible")
+
+# success, info
+st.sucess("Executed successfully")
+st.info("This is an information")
+st.warning("This is a warning")
+st.error("An error occured")
+channel = "iakdskjfdkj"
+st.write("Subscribe to my channel", channel)
+
+#Text input box
+name = st.text_input("Enter the Bible name", "Bible Name")
